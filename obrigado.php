dc8dbacd54561f7414b96a3f1108eb6572a9aaad
Marcos Santiago <santiago@ffxsistemas.com>
2018-05-08 15:10:47 -0300

    nothingchange

--- obrigado.php
+++ obrigado.php
@@ -6,18 +6,19 @@ $email = $_POST["Email"];
 $mensagem = $_POST["Mensagem"];
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=clientes',"root", "");
+    $pdo = new PDO('mysql:host=centraldoslead.mysql.dbaas.com.br;dbname=centraldoslead',"centraldoslead", "julio10");
      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $stmt = $pdo->prepare('INSERT INTO clientes (nome, telefone, email, mensagem) VALUES(:nome,:telefone,:email,:mensagem)');
+  $stmt = $pdo->prepare('INSERT INTO adt_leads(name, phone, email, message, date_created) VALUES(:nome,:telefone,:email,:mensagem, :date)');
   $stmt->execute(array(
       ':nome' => $nome,
       ':telefone' => $telefone,
       ':email' => $email,
-      ':mensagem' => $mensagem
+      ':mensagem' => $mensagem,
+      ':date' => date('Y-m-d H:i:s')
   ));
 
-  echo $stmt->rowCount();
+  echo "Contato Enviado com sucesso";
 } catch(PDOException $e) {
     echo "Ops! Erro ao processar sua solicitação.";
 }
