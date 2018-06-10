fe424ada33eb4d02c2c2c44014894f3d9259bcf0
Marcos Santiago <santiago@ffxsistemas.com>
2018-06-10 10:22:46 -0300

    fixed the merge failed in github

--- Landing_Page/index.php
+++ Landing_Page/index.php
@@ -565,7 +565,7 @@
                 <h3>Entre em contato e solicite um orçamento.</h3>
                 <br>
                 <div id="formulario" class="form-group">
-                    <form action="#">
+                    <form action="#" method="post">
                         <div class="input-group col-md-8">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                             <input id="nome" type="text" class="form-control" name="nome" placeholder="Nome">
@@ -578,12 +578,12 @@
                         <br>
                         <div class="input-group col-md-7">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                            <input id="email" type="email" class="form-control" name="Email" placeholder="email@.com.br">
+                            <input id="email" type="email" class="form-control" name="email" placeholder="email@.com.br">
                         </div>
                         <br>
                         <div class="tipo-servico">
                             <label for="sel1">Selecione o tipo de serviço</label>
-                            <select name="tp-servico" class="form-control" id="tp-servico">
+                            <select name="servico" class="form-control" id="tp-servico">
                                 <option value="residencial">Para Residência</option>
                                 <option value="empresarial">Para Empresa</option>
                             </select>
@@ -591,11 +591,11 @@
                         <br>
                         <div class="form-group">
                             <label for="mensagem">Mensagem:</label>
-                            <textarea class="form-control" rows="5" id="mensagem"></textarea>
+                            <textarea class="form-control" rows="5" name="mensagem" id="mensagem"></textarea>
                         </div>
 
                         <div class="form-group">
-                            <button type="button" class="btn btn-primary bt">Enviar</button>
+                            <button type="submit" class="btn btn-primary bt">Enviar</button>
                         </div>
 
                     </form>
